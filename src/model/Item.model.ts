84ba279340279f894ca56efdca55272dab1baed2
ali alzein <aliialzein1@gmail.com>
2025-09-24 20:39:59 +0300

    Added Models

---
+++ src/model/Item.model.ts
@@ -0,0 +1,7 @@
+export interface Item {
+    getCategory(): itemCategory;
+}
+
+export enum itemCategory {
+    CAKE,
+}
---
+++ src/model/Order.model.ts
@@ -0,0 +1,8 @@
+import { Item } from "./Item.model";
+
+export interface Order {
+    getItem(): Item,
+    getQuantity(): number,
+    getPrice(): number,
+    getId(): string;
+}
